8e527d35d14aef04b690b41c47e1e8e9c46840fa
Leo Turzi <leogturzi@gmail.com>
2022-12-23 06:32:32 -0700

    Footer creado

---
+++ src/layout/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer className='bg-light'>
+            <a href='https://github.com/leoturzi'>
+                <img
+                    src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
+                    alt=''
+                    className='github-icon'
+                />
+            </a>
+        </footer>
+    );
+}
+
+export default Footer;
