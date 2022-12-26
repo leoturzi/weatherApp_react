86fa3e7dd26fccab3114e94b68a9611024afe7d4
Leo Turzi <leogturzi@gmail.com>
2022-12-26 05:42:50 -0700

    SearchBar component con estilos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './SearchBar.css';
+import magnifyingGlass from '../assets/images/search.png';
 
 function SearchBar() {
     return (
         <div className='search-bar'>
-            <input type='text' />
-            <button className='search-bar_icon'>lupita</button>
+            <input type='text' id='search_bar' />
+            <button className='search-bar_icon'>
+                <img src={magnifyingGlass} alt='' />
+            </button>
         </div>
     );
 }
