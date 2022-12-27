fe13d9b81d1b20164398f8145e910412a4293a37
Leo Turzi <leogturzi@gmail.com>
2022-12-27 11:32:46 -0700

    Primera implementacion search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,15 +2,81 @@ import React from 'react';
 import './SearchBar.css';
 import magnifyingGlass from '../assets/images/search.png';
 
-function SearchBar() {
-    return (
-        <div className='search-bar'>
-            <input type='text' id='search_bar' />
-            <button className='search-bar_icon'>
-                <img src={magnifyingGlass} alt='' />
-            </button>
-        </div>
-    );
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+} from 'react-places-autocomplete';
+
+class LocationSearchInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { address: '' };
+    }
+
+    handleChange = (address) => {
+        this.setState({ address });
+    };
+
+    handleSelect = (address) => {
+        geocodeByAddress(address)
+            .then((results) => getLatLng(results[0]))
+            .then((latLng) => console.log('Success', latLng))
+            .catch((error) => console.error('Error', error));
+    };
+
+    render() {
+        return (
+            <PlacesAutocomplete
+                value={this.state.address}
+                onChange={this.handleChange}
+                onSelect={this.handleSelect}
+            >
+                {({
+                    getInputProps,
+                    suggestions,
+                    getSuggestionItemProps,
+                    loading,
+                }) => (
+                    <div className='autocomplete-input-container'>
+                        <input
+                            {...getInputProps({
+                                placeholder: 'Ciudad',
+                                className: 'location-search-input',
+                            })}
+                        />
+                        <div className='autocomplete-dropdown-container'>
+                            {loading && <div>Loading...</div>}
+                            {suggestions.map((suggestion) => {
+                                const className = suggestion.active
+                                    ? 'suggestion-item--active'
+                                    : 'suggestion-item';
+                                // inline style for demonstration purpose
+                                const style = suggestion.active
+                                    ? {
+                                          backgroundColor: '#fafafa',
+                                          cursor: 'pointer',
+                                      }
+                                    : {
+                                          backgroundColor: '#ffffff',
+                                          cursor: 'pointer',
+                                      };
+                                return (
+                                    <div
+                                        {...getSuggestionItemProps(suggestion, {
+                                            className,
+                                            style,
+                                        })}
+                                    >
+                                        <span>{suggestion.description}</span>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </PlacesAutocomplete>
+        );
+    }
 }
 
-export default SearchBar;
+export default LocationSearchInput;
--- src/layout/main/Main.jsx
+++ src/layout/main/Main.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Forecast from '../../components/Forecast';
 import MainCard from '../../components/MainCard';
 import SearchBar from '../../components/SearchBar';
 
 function Main() {
+    const [state, setState] = useState(null);
+
+    const updateState = () => {
+        setState((state, newState) => {
+            state = newState;
+        });
+    };
+
     return (
         <main className='container'>
             <MainCard />
